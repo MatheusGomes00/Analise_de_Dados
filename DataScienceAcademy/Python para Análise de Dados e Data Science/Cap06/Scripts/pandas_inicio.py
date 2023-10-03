88c2190f0bd8aedf25db46d44da3a2947d19431e
MatheusGomes notebook <matheus.recin@outlook.com>
2023-10-03 14:12:12 -0300

    Terça_03-10-23_14:12

--- DataScienceAcademy/Python para Análise de Dados e Data Science/Cap06/Scripts/pandas_inicio.py
+++ DataScienceAcademy/Python para Análise de Dados e Data Science/Cap06/Scripts/pandas_inicio.py
@@ -1,5 +1,5 @@
 import pandas as pd
 dataset = 'C:/Users/Matheus Gomes/Desktop/Análise de Dados/DataScienceAcademy/Python para Análise de Dados e Data Science/Cap06/arquivos/salarios.csv'
 data_frame = pd.read_csv(dataset)
-data_frame.head()
-
+print(data_frame.head())
+print(data_frame['Position Title'].value_counts())
